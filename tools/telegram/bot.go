ac02d845d9e1255c1f787d010df1547bcb555a98
Steven <shoutianyang@xibao100.com>
2018-09-14 13:28:51 +0800

    bot: trans huobi reply msgs to vi-VN.

--- tools/telegram/bot.go
+++ tools/telegram/bot.go
@@ -7,10 +7,10 @@ import (
 	"github.com/mkideal/log"
 	"github.com/panjf2000/ants"
 	"github.com/tokenme/tokenmed/common"
+	"github.com/tokenme/tokenmed/tools/shorturl"
 	"github.com/tokenme/tokenmed/tools/tracker"
 	"github.com/tokenme/tokenmed/utils/token"
 	"gopkg.in/telegram-bot-api.v4"
-	"github.com/tokenme/tokenmed/tools/shorturl"
 	"regexp"
 	"strconv"
 	"sync"
@@ -123,16 +123,16 @@ func (this *Bot) MessageConsumer(message *tgbotapi.Message, messageBuf chan Mess
 }
 
 func (this *Bot) VerifyCodeHandler(msg Message) {
-	var reply string
+	var reply, lang string
 	message := msg.Message
 	log.Info("Verify chat:%s, user:%s, code:%d", message.Chat.UserName, message.From.UserName, msg.Code)
 	db := this.Service.Db
 	rows, _, err := db.Query(`SELECT c.status, c.promotion_id, c.adzone_id, c.channel_id, c.promoter_id, c.airdrop_id, a.telegram_group, c.wallet, c.referrer, c.email, a.telegram_admin, a.reply_msg FROM tokenme.codes AS c LEFT JOIN tokenme.airdrops AS a ON (a.id=c.airdrop_id) WHERE c.id=%d LIMIT 1`, msg.Code)
 	if err != nil {
-		reply = "Sorry, we have some internal server bug :("
+		reply = il8n_trans("Sorry, we have some internal server bug :(", lang)
 	}
 	if len(rows) == 0 {
-		reply = "Sorry, submitted verification code is invalid or expired"
+		reply = il8n_trans("Sorry, submitted verification code is invalid or expired", lang)
 	} else {
 		codeStatus := rows[0].Uint(0)
 		proto := common.PromotionProto{
@@ -141,6 +141,9 @@ func (this *Bot) VerifyCodeHandler(msg Message) {
 			ChannelId: rows[0].Uint64(3),
 			UserId:    rows[0].Uint64(4),
 			AirdropId: rows[0].Uint64(5),
+		}
+		if proto.AirdropId == 51 { // 火币 空头ID
+			lang = "vi-VN" // 越南语
 		}
 		telegramGroupName := rows[0].Str(6)
 		wallet := rows[0].Str(7)
@@ -154,9 +157,9 @@ func (this *Bot) VerifyCodeHandler(msg Message) {
 			email = fmt.Sprintf("'%s'", db.Escape(email))
 		}
 		if telegramGroupName != message.Chat.UserName {
-			reply = fmt.Sprintf("Sorry, you must submit your code in group @%s", telegramGroupName)
+			reply = fmt.Sprintf(il8n_trans("Sorry, you must submit your code in group", lang) + " @%s", telegramGroupName)
 		} else if codeStatus == 0 {
-			reply = "Sorry, maybe you didn't submit your wallet address?"
+			reply = il8n_trans("Sorry, maybe you didn't submit your wallet address?", lang)
 		} else if codeStatus == 1 {
 			telegramUsername := db.Escape(message.From.UserName)
 			telegramUserId := strconv.FormatInt(int64(message.From.ID), 10)
@@ -166,16 +169,16 @@ func (this *Bot) VerifyCodeHandler(msg Message) {
 			_, ret, err := db.Query(`INSERT IGNORE INTO tokenme.airdrop_submissions (promotion_id, adzone_id, channel_id, promoter_id, airdrop_id, verify_code, email, wallet, telegram_msg_id, telegram_chat_id, telegram_user_id, telegram_chat_title, telegram_username, telegram_user_firstname, telegram_user_lastname, referrer) VALUES (%d, %d, %d, %d, %d, %d, %s, '%s', %d, %d, %s, '%s', '%s', '%s', '%s', '%s')`, proto.Id, proto.AdzoneId, proto.ChannelId, proto.UserId, proto.AirdropId, msg.Code, email, db.Escape(wallet), message.MessageID, message.Chat.ID, telegramUserId, db.Escape(message.Chat.UserName), telegramUsername, db.Escape(message.From.FirstName), db.Escape(message.From.LastName), db.Escape(referrer))
 			if err != nil {
 				log.Error(err.Error())
-				reply = "Sorry, we have some internal server bug :("
+				reply = il8n_trans("Sorry, we have some internal server bug :(", lang)
 			}
 			_, _, err = db.Query(`UPDATE tokenme.codes SET status=2 WHERE id=%d`, msg.Code)
 			if err != nil {
-				reply = "Sorry, we have some internal server bug :("
+				reply = il8n_trans("Sorry, we have some internal server bug :(", lang)
 			} else if ret.AffectedRows() == 0 {
-				reply = "Sorry, you already submitted in this airdrop and could not submit again"
+				reply = il8n_trans("Sorry, you already submitted in this airdrop and could not submit again", lang)
 			} else {
 				if replyMsg == "" {
-					reply = "Great! please wait for the airdrop transaction complete."
+					reply = il8n_trans("Great! please wait for the airdrop transaction complete.", lang)
 				} else {
 					reply = replyMsg
 				}
@@ -192,7 +195,7 @@ func (this *Bot) VerifyCodeHandler(msg Message) {
 				this.Tracker.Promotion.Submission(proto)
 			}
 		} else if codeStatus == 2 {
-			reply = "Your airdrop is in the pool, please wait for the transaction complete"
+			reply = il8n_trans("Your airdrop is in the pool, please wait for the transaction complete", lang)
 		}
 	}
 	replyMsg := tgbotapi.NewMessage(message.Chat.ID, reply)
