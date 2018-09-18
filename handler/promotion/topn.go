7e91ec9b3cc6c716a540b2d3dc1ca9e9314d64ff
Steven <shoutianyang@xibao100.com>
2018-09-18 10:27:38 +0800

    topn: remove email field.

--- handler/promotion/topn.go
+++ handler/promotion/topn.go
@@ -4,12 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	. "github.com/tokenme/tokenmed/handler"
 	"net/http"
-	"strings"
 )
 
 type TopNList struct {
 	Wallet string `json:"wallet" codec:"wallet"`
-	Email  string `json:"email" codec:"email"`
 	Cnt    int    `json:"cnt" codec:"cnt"`
 	Idx    int    `json:"idx" codec:"idx"`
 }
@@ -29,22 +27,13 @@ func TopNHandler(c *gin.Context) {
 	}
 
 	db := Service.Db
-	query := `SELECT DISTINCT 
-		c.wallet, c.email, IFNULL(s.cnt, 0) AS total_cnt
-FROM tokenme.codes AS c
-INNER JOIN (
-	SELECT referrer, count(*) AS cnt 
-	FROM tokenme.airdrop_submissions
-  WHERE airdrop_id = %d
-	GROUP BY referrer
-  ORDER BY cnt DESC, referrer
-  LIMIT %d
-) AS s ON s.referrer = c.wallet
-WHERE
-	c.airdrop_id = %d
-  AND c.email IS NOT NULL
-ORDER BY total_cnt DESC, c.email`
-	rows, _, err := db.Query(query, airdropId, n, airdropId)
+	query := `SELECT IF(asub.referrer IS NULL OR asub.referrer = "", asub.wallet, asub.referrer) AS referrer_s, count(*) AS cnt 
+	FROM tokenme.airdrop_submissions AS asub
+  WHERE asub.airdrop_id = %d
+	GROUP BY referrer_s
+  ORDER BY cnt DESC, referrer_s
+  LIMIT %d`
+	rows, _, err := db.Query(query, airdropId, n)
 	if CheckErr(err, c) {
 		return
 	}
@@ -53,8 +42,7 @@ ORDER BY total_cnt DESC, c.email`
 	for idx, row := range rows {
 		ret = append(ret, &TopNList{
 			Wallet: row.Str(0),
-			Email:  displayEmail(row.Str(1)),
-			Cnt:    row.Int(2),
+			Cnt:    row.Int(1),
 			Idx:    idx + 1,
 		})
 	}
@@ -66,9 +54,9 @@ ORDER BY total_cnt DESC, c.email`
 	}{}
 	query = `SELECT 
 	COUNT(1) AS total_cnt,
-	SUM(IF(TRIM(IFNULL(asub.referrer, "")) != "" AND asub.wallet != asub.referrer, 1, 0)) AS submissions
-FROM tokenme.airdrop_submissions AS asub 
-WHERE asub.airdrop_id = %d AND asub.status = 2`
+		SUM(IF(TRIM(IFNULL(asub.referrer, "")) != "" AND asub.wallet != asub.referrer, 1, 0)) AS submissions
+		FROM tokenme.airdrop_submissions AS asub
+		WHERE asub.airdrop_id = %d`
 	rows, _, err = db.Query(query, airdropId)
 	if CheckErr(err, c) {
 		return
@@ -84,28 +72,4 @@ WHERE asub.airdrop_id = %d AND asub.status = 2`
 		"topn":       ret,
 		"summary":    summary,
 	})
-}
-
-// 邮箱像是前两位及最后一位字符，及@后邮箱域名信息，如：ye****y@163.com
-func displayEmail(email string) string {
-	if email == "" {
-		return ""
-	}
-
-	vals := strings.Split(email, "@")
-	if len(vals) != 2 || len(vals[0]) < 2 {
-		return email
-	}
-
-	var pad rune = '*'
-	v := []rune(vals[0])
-	switch len(v) {
-	case 2, 3:
-		v[1] = pad
-	default:
-		for i := 2; i < len(v)-1; i++ {
-			v[i] = pad
-		}
-	}
-	return string(v) + "@" + vals[1]
 }
